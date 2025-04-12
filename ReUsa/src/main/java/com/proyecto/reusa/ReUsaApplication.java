6f9649e015914a7c2ea3c65399b989102f1a073b
O5CVR <oscarcasillasrodriguez@gmail.com>
2025-04-12 17:58:06 +0200

    Creación y configurtación del proyecto

---
+++ ReUsa/src/main/java/com/proyecto/reusa/ReUsaApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.reusa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReUsaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReUsaApplication.class, args);
+    }
+
+}
---
+++ ReUsa/src/test/java/com/proyecto/reusa/ReUsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.reusa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReUsaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
